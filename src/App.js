4255a37a7c3506bbdb19cd85363f0a904dc7ef3f
rizkisiraj <jomirwa@gmail.com>
2023-02-22 22:08:34 +0700

    membuat page merch

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 // import LoadingPage from './Pages/LoadingPage'
 import Intro from './Pages/Intro'
 import Home from './Pages/Home'
+import Merch from './Pages/Merch';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element = {<Intro/>} />
-        <Route path='/home' element = {<Home />} />
+        <Route path='/' element={<Intro/>} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/merch' element={<Merch />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Merch.jsx
@@ -0,0 +1,52 @@
+import Navbar1 from "../Components/Navbar";
+
+const Merch = () => {
+  return (
+    <>
+      <Navbar1 />
+      <div className="bg-[url('./assets/bg-home.png')] bg-center bg-cover w-full min-h-screen px-36 pt-24">
+        <div className="w-full flex gap-4 mb-8">
+          <button className="block min-w-[44px] min-h-[44px] px-4 bg-white rounded-lg">Kaos</button>
+          <button className="block min-w-[44px] min-h-[44px] px-4 bg-white rounded-lg">Jaket</button>
+          <button className="block min-w-[44px] min-h-[44px] px-4 bg-white rounded-lg">Totebag</button>
+          <button className="block min-w-[44px] min-h-[44px] px-4 bg-white rounded-lg">Aksesoris</button>
+          <button className="block min-w-[44px] min-h-[44px] px-4 bg-white rounded-lg">Celana</button>
+        </div>
+        <div className="w-full grid grid-cols-4 gap-16">
+            <div className="rounded-lg overflow-hidden bg-white min-h-[390px]">
+              <img src="https://picsum.photos/200" alt="card-img" className="w-full mb-2 h-52 object-cover block" />
+              <div>
+                <p className="text-lg font-semibold px-4">Naela Makan Nasi</p>
+              </div>
+            </div>
+            <div className="rounded-lg overflow-hidden bg-white min-h-[390px]">
+              <img src="https://picsum.photos/200" alt="card-img" className="w-full mb-2 h-52 object-cover block" />
+              <div>
+                <p className="text-lg font-semibold px-4">Naela Makan Nasi</p>
+              </div>
+            </div>
+            <div className="rounded-lg overflow-hidden bg-white min-h-[390px]">
+              <img src="https://picsum.photos/200" alt="card-img" className="w-full mb-2 h-52 object-cover block" />
+              <div>
+                <p className="text-lg font-semibold px-4">Naela Makan Nasi</p>
+              </div>
+            </div>
+            <div className="rounded-lg overflow-hidden bg-white min-h-[390px]">
+              <img src="https://picsum.photos/200" alt="card-img" className="w-full mb-2 h-52 object-cover block" />
+              <div>
+                <p className="text-lg font-semibold px-4">Naela Makan Nasi</p>
+              </div>
+            </div>
+            <div className="rounded-lg overflow-hidden bg-white min-h-[390px]">
+              <img src="https://picsum.photos/200" alt="card-img" className="w-full mb-2 h-52 object-cover block" />
+              <div>
+                <p className="text-lg font-semibold px-4">Naela Makan Nasi</p>
+              </div>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Merch;
