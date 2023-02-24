261240073599a6b690eef2eaebe7babf223fbd77
rizkisiraj <jomirwa@gmail.com>
2023-02-24 07:43:59 +0700

    inisialisasi controller untuk talent

---
+++ src/Components/Talent-controller.jsx
@@ -0,0 +1,11 @@
+const TalentController = () => {
+  return (
+    <aside className="flex">
+      <img src="./Risu.png" alt="" className="block w-[102px] h-[102px] mr-4" />
+      {/* <img src="" alt="" className="block w-[102px] mr-4" />
+      <img src="" alt="" className="block w-[102px] mr-4" /> */}
+    </aside>
+  )
+}
+
+export default TalentController;
--- src/Pages/Talent.jsx
+++ src/Pages/Talent.jsx
@@ -1,10 +1,12 @@
 import Navbar1 from "../Components/Navbar";
+import TalentController from "../Components/Talent-controller";
 
 
 const Talent = () => {
   return (
     <>
       <Navbar1 />
+      <TalentController />
     </>
   )
 }
