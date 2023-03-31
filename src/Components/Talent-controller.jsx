dd6dde688e0c2196616fe26a008477dd00f6db58
rizkisiraj <jomirwa@gmail.com>
2023-03-31 22:37:43 +0700

    recent changes on talent

--- src/Components/Talent-controller.jsx
+++ src/Components/Talent-controller.jsx
@@ -1,6 +1,5 @@
 import talents from '../Talents.json';
 
-
 const TalentController = ({ setIsShowing, setTalent, talent }) => {
   return (
     <aside className="flex flex-col md:flex-row mt-5 absolute top-10 md:top-auto right-5 md:bottom-10 md:right-28">
@@ -9,7 +8,7 @@ const TalentController = ({ setIsShowing, setTalent, talent }) => {
           return (
             <button key={tal.Nama} onClick={() => {
               setIsShowing(false);
-              setTalent(tal)
+              setTalent(tal);
               }} className='block mb-2 md:mr-4'>
               <img src={require(`../assets/${tal.Profile}`)} alt="Vio" className={`rounded-full object-cover ${talent.Nama === tal.Nama ? 'border-2 border-blue-200 w-[76px] md:w-[110px]' : null} block w-[68px] md:w-[102px]`} />
             </button>
