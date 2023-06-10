b602ae52e95d3ffbfbfb11d4f7f2c605ebef70c9
alamsyah08 <21523231@students.uii.ac.id>
2023-06-10 19:56:52 +0700

    tambahan button back, icon sosmed, revisi halaman intro

--- src/Pages/Intro.jsx
+++ src/Pages/Intro.jsx
@@ -8,16 +8,15 @@ function Intro() {
     const navigate = useNavigate()
     return (
         <div className="font-sans max-w-screen h-fit max-md:h-screen">
-            <div className="w-full xl:h-screen max-lg:h-[50vh] bg-black flex items-center">
+            <div className="w-full h-screen bg-black flex items-center">
                 <video className="w-screen xl:h-screen max-xl:aspect-video object-cover object-center" muted={true} autoPlay loop >
                     <source src={MyVideo} type="video/webm" />
                 </video>
             </div>
-            <div className="bg-[url('./assets/bg-journey.webp')] bg-cover bg-center lg:h-screen max-lg:h-[50vh] w-full flex justify-center items-center">
-                <button className=' w-[278px] max-lg:w-1/3 max-[500px]:w-1/2 h-20 max-md:h-14 bg-white/50 hover:bg-white/20 rounded-2xl border-white border-2 shadow-blue-800 shadow-md' onClick={() => navigate('/home')}>
-                    <span style={{ textShadow: '0px 2px 2px rgb(47 79 79) ' }} className="text-white font-semibold text-[24px] max-lg:text-xl max-md:text-lg max-sm:text-base max-[400px]:text-sm max-[300px]:text-xs">Start Your Journey</span>
-                </button>
-            </div>
+
+            <button className='absolute bottom-10 left-1/2 transform -translate-x-1/2 bottom  w-[278px] max-lg:w-1/3 max-[500px]:w-1/2 h-20 max-md:h-14 bg-gray-700 hover:bg-white/20 rounded-2xl border-white border-2 shadow-blue-800 shadow-md' onClick={() => navigate('/home')}>
+                <span style={{ textShadow: '0px 2px 2px rgb(47 79 79) ' }} className="text-white font-semibold text-[24px] max-lg:text-xl max-md:text-lg max-sm:text-base max-[400px]:text-sm max-[300px]:text-xs">Start Your Journey</span>
+            </button>
         </div>
 
     )
